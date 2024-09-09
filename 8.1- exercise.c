b1fbebf6e02003262ac660cc8f92fad2e6198cb1
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-09-09 18:17:02 +0200

    leared about trylock

--- 8.1- exercise.c
+++ 8.1- exercise.c
@@ -1,3 +1,14 @@
+/* 
+we will have 2 threads.
+- I have this array:
+int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+- what we want the program do is :
+- to sume up the numbers in the primes array.
+we have to send the first half to the first
+thread, and the second half to the second thread.
+- so we will get two sums in main, and we will sum them up.
+ */
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +30,8 @@ void *sum(void *arg)
     }
     printf("index: %d\n", *index);
     printf("sum: %d\n", sum);
-    *(int *)arg = sum;
+    *(int *)arg = sum; // here arg is a pointer to a void pointer, by (int *), we are casting it to a pointer to an integer
+                       // and then we are dereferencing it and we access the value of the integer, then we assign the sum to it.
     return(arg);
 }
 
