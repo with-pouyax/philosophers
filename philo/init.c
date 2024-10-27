8d7ce782d0fb0d565818cd31827df6b14a45f612
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-10-27 19:36:04 +0100

    added some comments

--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: pouyaximac <pouyaximac@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 16:00:00 by pouyaximac        #+#    #+#             */
-/*   Updated: 2024/10/27 10:28:09 by pouyaximac       ###   ########.fr       */
+/*   Updated: 2024/10/27 18:02:27 by pouyaximac       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,24 @@
 
 int	parse_arguments(int argc, char **argv, t_data *data)
 {
-	if (argc < 5 || argc > 6)
+	if (argc < 5 || argc > 6) // if the number of arguments is less than 5 or more than 6, print an error message and return 0
 	{
 		write(2, "Error: Invalid number of arguments.\n", 36);
 		return (0);
 	}
-	if (!validate_arguments(argc, argv))
+	if (!validate_arguments(argc, argv)) // if the arguments are not valid, return 0
 		return (0);
-	data->num_philosophers = atoi(argv[1]);
-	data->time_to_die = ft_atoll(argv[2]);
-	data->time_to_eat = ft_atoll(argv[3]);
-	data->time_to_sleep = ft_atoll(argv[4]);
+	data->num_philosophers = ft_atoll(argv[1]); // convert the first argument to an integer and assign it to the num_philosophers field of the data struct
+	if (data->num_philosophers > 200) // if the number of philosophers is greater than 200, print an error message and return 0
+	{
+		write(2, "Error: Number of philosophers must be less than 200.\n", 54);
+		return (0);
+	}
+	data->time_to_die = ft_atoll(argv[2]); // convert the second argument to a long long and assign it to the time_to_die field of the data struct
+	data->time_to_eat = ft_atoll(argv[3]); // convert the third argument to a long long and assign it to the time_to_eat field of the data struct
+	data->time_to_sleep = ft_atoll(argv[4]); // convert the fourth argument to a long long and assign it to the time_to_sleep field of the data struct
 	if (argc == 6)
-		data->num_meals = atoi(argv[5]);
+		data->num_meals = ft_atoll(argv[5]);
 	else
 		data->num_meals = -1;
 	return (1);
@@ -38,14 +43,14 @@ int	validate_arguments(int argc, char **argv)
 	int			i;
 
 	i = 1;
-	while (i < argc)
+	while (i < argc) // iterate through the arguments 
 	{
-		if (!is_valid_number(argv[i]))
+		if (!is_valid_number(argv[i])) // if the argument is not a valid number, print an error message and return 0
 		{
 			write(2, "Error: Invalid argument.\n", 25);
 			return (0);
 		}
-		arg_value = ft_atoll(argv[i]);
+		arg_value = ft_atoll(argv[i]); // convert the argument to a long long 
 		if (arg_value <= 0 || arg_value > 4294967295)
 		{
 			write(2, "Error: Argument out of valid range.\n", 35);
@@ -53,7 +58,7 @@ int	validate_arguments(int argc, char **argv)
 		}
 		i++;
 	}
-	return (1);
+	return (1); // if all the arguments are valid, return 1
 }
 
 static int	initialize_mutexes(t_data *data)
--- philo/utils.c
+++ philo/utils.c
@@ -6,7 +6,7 @@
 /*   By: pouyaximac <pouyaximac@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 17:15:00 by pouyaximac        #+#    #+#             */
-/*   Updated: 2024/10/26 11:52:09 by pouyaximac       ###   ########.fr       */
+/*   Updated: 2024/10/27 16:12:16 by pouyaximac       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ long long	ft_atoll(const char *str)
 	result = 0;
 	sign = 1;
 	i = 0;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)) // skip whitespace characters
 		i++;
-	if (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-' || str[i] == '+') // check if the number is negative
 	{
-		if (str[i++] == '-')
-			sign = -1;
+		if (str[i++] == '-') // if the number is negative, set the sign to -1
+			sign = -1; 
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9') // iterate through the string
 	{
-		overflow = check_overflow(result, sign, str[i]);
+		overflow = check_overflow(result, sign, str[i]); // check if adding the next digit will cause an overflow
 		if (overflow != 1)
 			return (overflow);
 		result = result * 10 + (str[i++] - '0');
@@ -41,15 +41,17 @@ long long	ft_atoll(const char *str)
 
 int	check_overflow(long long result, int sign, char c)
 {
-	if (result > LLONG_MAX / 10)
+	if (result > LLONG_MAX / 10) // if result is greater than LLONG_MAX / 10, then adding the next digit will cause an overflow
 	{
-		if (sign == 1)
+		if (sign == 1) // if the number is positive, return -1
 			return (-1);
 		else
-			return (0);
+			return (0); // if the number is negative, return 0
 	}
 	if (result == LLONG_MAX / 10 &&
-		(c - '0') > (LLONG_MAX % 10))
+		(c - '0') > (LLONG_MAX % 10)) /* if LLONG_MAX / 10 is exactly equal to result then adding the next digit
+		will cause an overflow only if the next digit is greater than LLONG_MAX % 10 because LLONG_MAX % 10 is the
+		last digit of LLONG_MAX */
 	{
 		if (sign == 1)
 			return (-1);
@@ -59,22 +61,22 @@ int	check_overflow(long long result, int sign, char c)
 	return (1);
 }
 
-int	is_valid_number(const char *str)
+int	is_valid_number(const char *str) // check if the string is a valid number
 {
 	int	i;
 
 	i = 0;
-	if (str[i] == '+' || str[i] == '-')
+	if (str[i] == '+' || str[i] == '-') // if the first character is a + or a -, skip it
 		i++;
-	if (str[i] == '\0')
+	if (str[i] == '\0') // if the first character is the null terminator, return 0
 		return (0);
-	while (str[i])
+	while (str[i]) // iterate through the string
 	{
-		if (str[i] < '0' || str[i] > '9')
+		if (str[i] < '0' || str[i] > '9') // if the character is not a number, return 0
 			return (0);
-		i++;
+		i++; // otherwise, go to the next character
 	}
-	return (1);
+	return (1); // if all characters are numbers, return 1
 }
 
 long long	get_time_in_ms(void)
